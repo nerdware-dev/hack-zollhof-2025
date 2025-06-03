44a4dd6431ec8d550217bc2d2321c8f21005a1b0
Franziska <Franziska.Roder@gmx.de>
2025-06-03 15:01:09 +0200

    activity

--- fe/src/components/activity/activity.tsx
+++ fe/src/components/activity/activity.tsx
@@ -11,7 +11,7 @@ export default function ActivityComponent({ activity }: { activity: Activity }) 
                 </p>
                 <h1>{activity.title}</h1>
                 <p>{activity.description}</p>
-                <button>Anmelden</button>
+                <button className="rounded-xl border-solid border-black">Anmelden</button>
                 <button><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=star" /></button>
             </div>
             <div>
